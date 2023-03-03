9807d2f37c8b04d78abdcae814e1293f42d9c92e
andy <andres.genaisir@gmail.com>
2023-03-02 23:06:46 -0300

    Creación componente redes y estructuración

--- FrontEndAP/src/app/app.module.ts
+++ FrontEndAP/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
