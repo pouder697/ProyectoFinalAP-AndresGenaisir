4c066e70add3e40909657d033aae9c42cb3a9583
andy <andres.genaisir@gmail.com>
2023-03-09 20:38:33 -0300

    re-estructuración header.comp + agregado y estilización about.comp

--- FrontEndAP/src/app/app.module.ts
+++ FrontEndAP/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RedesComponent
+    RedesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
